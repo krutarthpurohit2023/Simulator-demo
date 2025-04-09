0f82f6a0e521c054aa2dbd56301b7bbcca5dd742
krutarthpurohit2023 <155361585+krutarthpurohit2023@users.noreply.github.com>
2025-04-09 12:23:36 +0530

    Relesing: v2.0.0

--- main.c
+++ main.c
@@ -3,7 +3,9 @@
 int main(void)
 {
     printf("Hello, World! Krutarth Here\n");
-    printf("This is a test file and updating the version in next commit\n");
+    printf("This is a test file and updating the version in this commit\n");
     printf("This is BacancySystems\n");
+
+    printf("New release-2.0.0\n");
     return 0;
 }
